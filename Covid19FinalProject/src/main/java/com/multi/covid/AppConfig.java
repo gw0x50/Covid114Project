1f7069bf43c6851fc53f9f92a791e6d96a384ffc
melonbottle <melonbottle@gmail.com>
2021-05-20 15:41:20 +0900

    branch test

--- Covid19FinalProject/src/main/java/com/multi/covid/AppConfig.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/AppConfig.java
@@ -3,6 +3,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+// 주석 테스트하기
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
 
