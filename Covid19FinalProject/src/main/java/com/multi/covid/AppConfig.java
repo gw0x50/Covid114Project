b03eaf793cca7fe55ab482fd98656acfc47726ff
Jooahru <dndlf333@naver.com>
2021-05-20 15:40:34 +0900

    add wooil

--- Covid19FinalProject/src/main/java/com/multi/covid/AppConfig.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/AppConfig.java
@@ -3,6 +3,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//주석추가 -우일
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
 
