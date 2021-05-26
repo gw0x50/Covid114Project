8426488e416de870c62c5f0bc17948d026d1176a
Jooahru <dndlf333@naver.com>
2021-05-26 14:38:04 +0900

    add allcenter marker,event

--- Covid19FinalProject/src/main/java/com/multi/covid/controller/WebController.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/controller/WebController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.multi.covid.domain.CenterVO;
 import com.multi.covid.domain.ResultVO;
 import com.multi.covid.service.ChartService;
 import com.multi.covid.service.MapService;
@@ -19,10 +20,14 @@ public class WebController {
 	private ChartService chartService;
 	@Autowired
 	private MapService mapService;
+	
+	
 
 	@RequestMapping("/")
 	public ModelAndView main() {
 		ModelAndView mv = new ModelAndView();
+		List<CenterVO> allcenter = mapService.getAllCenter();
+		mv.addObject("allcenter",allcenter);
 
 		mv.setViewName("index");
 
