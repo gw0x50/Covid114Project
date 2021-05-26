6b0b69ec619081d354641e45e5a8c26b9ae60a4a
melonbottle <melonbottle@gmail.com>
2021-05-26 16:34:14 +0900

    chart 구현 및 controller, service 수정

--- Covid19FinalProject/src/main/java/com/multi/covid/controller/ChartController.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/controller/ChartController.java
@@ -20,48 +20,21 @@ public class ChartController {
 	@Autowired
 	private ChartService service;
 
-	@RequestMapping("/charttest")
-	public ModelAndView charttest() {
-		ModelAndView mv = new ModelAndView();
-
-		mv.setViewName("charttest");
-
-		// LiveVO liveVO =  liveService.getOneLive("2021-05-18");
-		// liveVO.calSum();
-		// System.out.println(liveVO);
-
-		// List<CenterVO> centerList = centerService.getAllCenter();
-		// for(CenterVO a : centerList) System.out.println(a);
-
-		List<ResultVO> sevenDays = service.get7DaysResult("합계");
-		List<ResultVO> fourWeeks = service.get4WeeksResult("합계");
-		List<ResultVO> twelveMonths = service.get12MonthsResult("합계");
-		System.out.println(sevenDays);
-		System.out.println(fourWeeks);
-		System.out.println(twelveMonths);
-
-		mv.addObject("sevenDays", sevenDays);
-		mv.addObject("fourWeeks", fourWeeks);
-		mv.addObject("twelveMonths", twelveMonths);
-
-		return mv;
-	}
-
 	@RequestMapping(value = "/getValues", method = RequestMethod.GET)
 	@ResponseBody
 	public String getValues(String type, String location) {
 		Gson gson = new Gson();
 		List<ResultVO> list = new ArrayList<ResultVO>();
-		
-		if(location.equals("전체")) location = "합계";
-		
+
+		if (location.equals("전체")) location = "합계";
+
 		if (type.equals("일별")) {
 			list = service.get7DaysResult(location);
 		}
-		if (type.equals("주별")) {
+		else if (type.equals("주별")) {
 			list = service.get4WeeksResult(location);
 		}
-		if (type.equals("월별")) {
+		else if (type.equals("월별")) {
 			list = service.get12MonthsResult(location);
 		}
 
