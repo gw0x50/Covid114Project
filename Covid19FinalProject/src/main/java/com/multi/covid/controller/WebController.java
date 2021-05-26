ef695d14757a9941662f251816e8ab6a10a2310a
melonbottle <melonbottle@gmail.com>
2021-05-26 16:38:34 +0900

    merge 이후 에러 코드 수정

--- Covid19FinalProject/src/main/java/com/multi/covid/controller/WebController.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/controller/WebController.java
@@ -1,21 +1,19 @@
 package com.multi.covid.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.multi.covid.domain.CenterVO;
-import com.multi.covid.domain.ResultVO;
-import com.multi.covid.service.ChartService;
 import com.multi.covid.service.MapService;
 
 @Controller
 public class WebController {
 	@Autowired
 	private MapService mapService;
-	
-	
 
 	@RequestMapping("/")
 	public ModelAndView main() {
