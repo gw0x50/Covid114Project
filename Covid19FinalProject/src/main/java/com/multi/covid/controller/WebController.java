494a5a7413cca2db51976ff47f618a4fcad93cbd
Jooahru <dndlf333@naver.com>
2021-05-27 14:26:55 +0900

    Change ClientID & import List at WebController

--- Covid19FinalProject/src/main/java/com/multi/covid/controller/WebController.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/controller/WebController.java
@@ -1,13 +1,13 @@
 package com.multi.covid.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.multi.covid.domain.CenterVO;
-import com.multi.covid.domain.ResultVO;
-import com.multi.covid.service.ChartService;
 import com.multi.covid.service.MapService;
 
 @Controller
