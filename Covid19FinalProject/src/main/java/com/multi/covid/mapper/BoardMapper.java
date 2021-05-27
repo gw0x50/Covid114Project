801060b3a1ccc2fa4a00db275b5fd7da99c69a39
melonbottle <melonbottle@gmail.com>
2021-05-27 20:10:41 +0900

    board 기능 구현

--- Covid19FinalProject/src/main/java/com/multi/covid/mapper/BoardMapper.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/mapper/BoardMapper.java
@@ -1,12 +1,16 @@
 package com.multi.covid.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import com.multi.covid.domain.LiveVO;
+import com.multi.covid.domain.ResultVO;
 
 @Mapper
 @Repository
 public interface BoardMapper {
-	LiveVO getOneLive(String date);
+	LiveVO getRecentLive();
+	List<ResultVO> getRecentTwoResult(String location);
 }
