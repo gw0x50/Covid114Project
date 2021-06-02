9a16bcde3de977cdae9e24f5f7f7c0efe66ba8eb
melonbottle <melonbottle@gmail.com>
2021-06-02 16:56:37 +0900

    일일 그래프 위 값 표시

--- Covid19FinalProject/src/main/resources/static/js/chart.js
+++ Covid19FinalProject/src/main/resources/static/js/chart.js
@@ -50,7 +50,6 @@ $(document).ready(function() {
 				});
 				if (chartLabels.length == 7) {
 					myChart = new Chart(ctx, {
-
 						type: 'bar',
 						data: {
 							labels: chartLabels,
@@ -97,6 +96,32 @@ $(document).ready(function() {
 								}
 							}
 						},
+						plugins: [{
+							afterDatasetsDraw: function(chart) {
+								var ctx = chart.ctx;
+								chart.data.datasets.forEach(function(dataset, i) {
+									var meta = chart.getDatasetMeta(i);
+									if (!meta.hidden) {
+										meta.data.forEach(function(element, index) {
+											// Draw the text in black, with the specified font
+											ctx.fillStyle = 'rgb(0, 0, 0)';
+											var fontSize = 15;
+											var fontStyle = 'normal';
+											var fontFamily = 'Helvetica Neue';
+											ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
+											// Just naively convert to string for now
+											var dataString = dataset.data[index].toString();
+											// Make sure alignment settings are correct
+											ctx.textAlign = 'center';
+											ctx.textBaseline = 'middle';
+											var padding = 5;
+											var position = element.tooltipPosition();
+											ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding);
+										});
+									}
+								});
+							}
+						}]
 					});
 				}
 				else if (chartLabels.length == 4) {
