67e0a2c79bc258f91742c9c2fa73aa56d2e8da83
louis-25 <20151577@vision.hoseo.edu>
2021-06-02 17:47:03 +0900

    불필요한 코드정리

--- Covid19FinalProject/src/main/java/com/multi/covid/service/AISpeakerService.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/service/AISpeakerService.java
@@ -1,7 +1,6 @@
 package com.multi.covid.service;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 
 import org.json.simple.parser.ParseException;
@@ -13,11 +12,10 @@ import com.multi.covid.domain.ResultVO;
 public interface AISpeakerService {	
 	LiveVO getOneLive(String date);
 	List<CenterVO> getAllCenter();
-	List<ResultVO> getOneResult(String date);
-	List<ResultVO> getBetweenResult(HashMap<String, String> map);
+	List<ResultVO> getOneResult(String date);	
 	List<ResultVO> getAllResult();
-	String patient(String day, String location); //일일 확진자수	
-	List<String> geolocation(String r1, String r2, String r3); //가까운 진료소
-	String weather(String lat, String lon) throws IOException, ParseException; //스피커 접속지역의 날씨정보	
+	String getPatient(String day, String location); //일일 확진자수	
+	List<String> getGeolocation(String r1, String r2, String r3); //가까운 진료소
+	String getWeather(String lat, String lon) throws IOException, ParseException; //스피커 접속지역의 날씨정보	
 	
 }
