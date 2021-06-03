50bceb29fb8f96978a6ea68fd228711dda0498b3
Jooahru <dndlf333@naver.com>
2021-06-03 17:41:43 +0900

    change map css and marker image

--- Covid19FinalProject/src/main/resources/static/js/map.js
+++ Covid19FinalProject/src/main/resources/static/js/map.js
@@ -3,12 +3,17 @@ $(document).ready(function() {
 	var infoWindows = [];
 	var infoWindow;
 	var seqnum;
-	var closenum=0;
+	var closenum = 0;
 	var locallat = 37.5666805
 	var locallng = 126.9784147;
+	var marker;
 	var marker1;
+	var marker_address;
 	var infoWindowlocal = new naver.maps.InfoWindow({
-		anchorSkew: true
+		borderWidth: 0,
+		disableAnchor: true,
+		backgroundColor: 'transparent',
+		pixelOffset: new naver.maps.Point(0, -35)
 	});
 
 
@@ -19,50 +24,48 @@ $(document).ready(function() {
 		mapTypeControl: true
 	})
 
-	var infowindow1 = new naver.maps.InfoWindow();
+	var infowindow1 = new naver.maps.InfoWindow({
+		borderWidth: 0,
+		disableAnchor: true,
+		backgroundColor: 'transparent',
+		pixelOffset: new naver.maps.Point(0, -40)
+	});
 
 
 	map.setCursor('pointer');
 
 	function onSuccessGeolocation(position) {
 		var location = new naver.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
 		locallat = position.coords.latitude;
 		locallng = position.coords.longitude;
 		getAllCenter();
-
 		map.setCenter(location);
 		map.setZoom(14);
-
-		infowindow1.setContent('<div style="padding:20px;"><center><h4 >현재 위치 주변 <br>백신센터를 표시하였습니다.</h4></center><br /></div>');
-		 marker1 = new naver.maps.Marker({
+		infowindow1.setContent('<div class="map_localwindow"><center><strong>현재 위치 주변 <br>백신센터를 표시하였습니다.</strong></center></div>');
+		marker1 = new naver.maps.Marker({
 			position: new naver.maps.LatLng(position.coords.latitude, position.coords.longitude),
 			map: map,
-			    icon: {
-        content: '<img src="resources/images/pin_default.png" alt="" ' +
-                 'style="margin: 0px; padding: 0px; border: 0px solid transparent; display: block; max-width: none; max-height: none; ' +
-                 '-webkit-user-select: none; position: absolute; width: 22px; height: 35px; left: 0px; top: 0px;">',
-        size: new naver.maps.Size(22, 35),
-        anchor: new naver.maps.Point(11, 35)
-    }
-			
+			icon: {
+				content: '<img src="resources/images/localpin.jpg" alt="" ' +
+					'style="margin: 0px; padding: 0px; border: 0px solid transparent; display: block; max-width: none; max-height: none; ' +
+					'-webkit-user-select: none; position: absolute; width: 22px; height: 35px; left: 0px; top: 0px;">',
+				size: new naver.maps.Size(22, 35),
+				anchor: new naver.maps.Point(11, 35)
+			}
 		});
-		
 		infowindow1.open(map, location);
-		
 	} //onSuccessGeolocation end
 
 	function getClickHandler(seq) {
 		return function(e) {
 			var marker = markers[seq],
 				infoWindow = infoWindows[seq];
-
 			if (infoWindow.getMap()) {
 				infoWindow.close();
-				closenum=0;
+				closenum = 0;
 			} else {
 				infoWindow.open(map, marker);
-				closenum=1;
+				closenum = 1;
 				seqnum = seq;
 			}
 		}
@@ -76,14 +79,14 @@ $(document).ready(function() {
 			type: "post",
 			success: function(allcenter) {
 				$.each(allcenter, function(key, value) {
-					
-					var marker = new naver.maps.Marker({
+
+					marker = new naver.maps.Marker({
 						position: new naver.maps.LatLng(value.lat, value.lng),
 						map: map
 					});
 
 					var contentStrings = [
-						'<div class="iw_inner">',
+						'<div class="map_centerwindow">',
 						'<center>',
 						'   <h3>' + value.center_name + '</h3>',
 						'<hr></center>',
@@ -91,11 +94,13 @@ $(document).ready(function() {
 						'      <strong>전화번호</strong> : ' + value.phone_number + '<br />',
 						'</div>'
 					].join('');
-
 					infoWindow = new naver.maps.InfoWindow({
-						content: contentStrings
+						content: contentStrings,
+						borderWidth: 0,
+						disableAnchor: true,
+						backgroundColor: 'transparent',
+						pixelOffset: new naver.maps.Point(0,0)
 					});
-
 					markers.push(marker);
 					infoWindows.push(infoWindow);
 				});
@@ -107,9 +112,7 @@ $(document).ready(function() {
 	} //getallCenter end
 
 	function searchCoordinateToAddress(latlng) {
-
 		infoWindowlocal.close();
-
 		naver.maps.Service.reverseGeocode({
 			coords: latlng,
 			orders: [
@@ -120,17 +123,25 @@ $(document).ready(function() {
 			if (status === naver.maps.Service.Status.ERROR) {
 				return alert('Something Wrong!');
 			}
-
 			infoWindowlocal.setContent([
-				'<div style="padding:10px;">',
-				'<center><h4 >선택한 장소 주변 <br>백신센터를 표시하였습니다.</h4></center><br />',
+				'<div class="map_localwindow">',
+				'<center><strong>선택한 장소 주변 <br>백신센터를 표시하였습니다.</strong></center>',
 				'</div>'
 			].join('\n'));
+			marker_address = new naver.maps.Marker({
+				position: new naver.maps.LatLng(latlng.y, latlng.x),
+				map: map,
+				icon: {
+					content: '<img src="resources/images/pin_default.png" alt="" ' +
+						'style="margin: 0px; padding: 0px; border: 0px solid transparent; display: block; max-width: none; max-height: none; ' +
+						'-webkit-user-select: none; position: absolute; width: 22px; height: 35px; left: 0px; top: 0px;">',
+					size: new naver.maps.Size(22, 35),
+					anchor: new naver.maps.Point(11, 35)
+				}
+			});
 			locallat = latlng.y;
 			locallng = latlng.x;
 			getAllCenter();
-
-
 			infoWindowlocal.open(map, latlng);
 		});
 	}
@@ -142,59 +153,57 @@ $(document).ready(function() {
 			if (status === naver.maps.Service.Status.ERROR) {
 				return alert('Something Wrong!');
 			}
-
 			if (response.v2.meta.totalCount === 0) {
 				return alert('지번주소 혹은 도로명 주소를 입력하세요');
 			}
-
 			var htmlAddresses = [],
 				item = response.v2.addresses[0],
 				point = new naver.maps.Point(item.x, item.y);
+			marker_address = new naver.maps.Marker({
+				position: new naver.maps.LatLng(item.y, item.x),
+				map: map,
+				icon: {
+					content: '<img src="resources/images/pin_default.png" alt="" ' +
+						'style="margin: 0px; padding: 0px; border: 0px solid transparent; display: block; max-width: none; max-height: none; ' +
+						'-webkit-user-select: none; position: absolute; width: 22px; height: 35px; left: 0px; top: 0px;">',
+					size: new naver.maps.Size(22, 35),
+					anchor: new naver.maps.Point(11, 35)
+				}
+			});
 			locallat = item.y;
 			locallng = item.x;
 			getAllCenter();
-
 			if (item.roadAddress) {
 				htmlAddresses.push('[도로명 주소] ' + item.roadAddress);
 			}
-
 			if (item.jibunAddress) {
 				htmlAddresses.push('[지번 주소] ' + item.jibunAddress);
 			}
-
 			if (item.englishAddress) {
 				htmlAddresses.push('[영문명 주소] ' + item.englishAddress);
 			}
-
 			infoWindowlocal.setContent([
-				'<div style="padding:10px;">',
-				'<center><h4 >검색하신 주소 주변 <br>백신센터를 표시하였습니다.</h4></center><br />',
+				'<div class="map_localwindow">',
+				'<center><strong>검색하신 주소 주변 <br>백신센터를 표시하였습니다.</strong></center>',
 				'</div>'
 			].join('\n'));
-
 			map.setCenter(point);
 			infoWindowlocal.open(map, point);
 		});
 	}
 
-
-
 	map.addListener('click', function(e) {
 		console.log(closenum);
-		if (closenum==1) {
+		if (closenum == 1) {
 			infoWindows[seqnum].close();
-			closenum=0;
-
+			closenum = 0;
 		} else {
 			searchCoordinateToAddress(e.coord);
-			
 		}
 	});
 
 	$('#map_address').on('keydown', function(e) {
 		var keyCode = e.which;
-
-
 		if (keyCode === 13) { // Enter Key
 			if ($('#map_address').val() != '') {
 				searchAddressToCoordinate($('#map_address').val());
@@ -214,8 +223,18 @@ $(document).ready(function() {
 	function onErrorGeolocation() {
 		var center = map.getCenter();
 		getAllCenter();
-		infowindow1.setContent('<div style="padding:20px;">  기본 설정위치(역삼멀티캠퍼스) </div>');
-
+		marker1 = new naver.maps.Marker({
+			position: new naver.maps.LatLng(37.5666805, 126.9784147),
+			map: map,
+			icon: {
+				content: '<img src="resources/images/localpin.jpg" alt="" ' +
+					'style="margin: 0px; padding: 0px; border: 0px solid transparent; display: block; max-width: none; max-height: none; ' +
+					'-webkit-user-select: none; position: absolute; width: 22px; height: 35px; left: 0px; top: 0px;">',
+				size: new naver.maps.Size(22, 35),
+				anchor: new naver.maps.Point(11, 35)
+			}
+		});
+		infowindow1.setContent('<div class="localwindow">   기본 설정위치(역삼멀티캠퍼스) </div>');
 		infowindow1.open(map, center);
 	}
 
@@ -227,13 +246,19 @@ $(document).ready(function() {
 		else {
 			var center = map.getCenter();
 			getAllCenter();
-			infowindow1.setContent('<div style="padding:20px;">  기본 설정위치(역삼멀티캠퍼스) </div>');
-
+			marker1 = new naver.maps.Marker({
+				position: new naver.maps.LatLng(37.5666805, 126.9784147),
+				map: map,
+				icon: {
+					content: '<img src="resources/images/localpin.jpg" alt="" ' +
+						'style="margin: 0px; padding: 0px; border: 0px solid transparent; display: block; max-width: none; max-height: none; ' +
+						'-webkit-user-select: none; position: absolute; width: 22px; height: 35px; left: 0px; top: 0px;">',
+					size: new naver.maps.Size(22, 35),
+					anchor: new naver.maps.Point(11, 35)
+				}
+			});
+			infowindow1.setContent('<div class="localwindow">  기본 설정위치(역삼멀티캠퍼스) </div>');
 			infowindow1.open(map, center);
 		}
 	});
-
-	
-
-
 }); //ready 함수 end
