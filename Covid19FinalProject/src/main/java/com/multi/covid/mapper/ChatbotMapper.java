98d5aa745db9b29ed2ef796f315356e2fe31be1e
2damlee <2damlee@gmail.com>
2021-06-05 00:12:37 +0900

    ChatbotMapper 안 쓰는 메소드 제거, 이름 변경, covid_vaccine_center 조회문 추

--- Covid19FinalProject/src/main/java/com/multi/covid/mapper/ChatbotMapper.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/mapper/ChatbotMapper.java
@@ -13,14 +13,14 @@ import com.multi.covid.domain.ResultVO;
 @Mapper
 @Repository
 public interface ChatbotMapper {
-	ResultVO getOneResult(String location);
-	int getLocLive(HashMap<String, String> map);
-	LiveVO getOneLive(String date);
-	List<CenterVO> getAllCenter();
-	List<CenterVO> getLocCenter(String location);
-	List<String> getAddrTwo(String location);
-	List<String> getAddrThree(String address_two);
-	List<String> getAddrFour(String address_three);
-	List<CenterVO> getAddrCenter(String address_three);
-	List<CenterVO> getFacility(String facility_name);
+	ResultVO getResult(String location); // 누적 확진자 수 조회 
+	LiveVO getLive(String date); // 실시간 확진자 수 조회 
+	int getLocLive(HashMap<String, String> map); // 실시간 지역 확진자 수 조회 
+	List<CenterVO> getLocCenter(String location); // 지역별 센터 조회 
+	List<String> getAddrTwo(String location); // 주소 두 번째 값 조회
+	List<String> getAddrThree(String address_two); //주소 세 번째 값 조회 
+	List<String> getAddrFour(String address_three); //주소 네 번째 값 조회 
+	List<CenterVO> getAddrCenter(String address_three); // 주소로 백신센터 조회
+	List<CenterVO> getFacility(String facility_name); // 시설 이름으로 백신센터 조회
+	List<CenterVO> getFacility_loc(HashMap<String, String> map); 
 }
