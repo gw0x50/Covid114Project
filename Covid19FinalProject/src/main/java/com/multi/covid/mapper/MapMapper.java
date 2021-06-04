bd7c5aa550109b5b8e4ad6e8a4964fbaaccf3a62
Jooahru <dndlf333@naver.com>
2021-06-04 17:11:50 +0900

    service controller 미사용 코드 삭제 및 map.css 중복코드 삭제

--- Covid19FinalProject/src/main/java/com/multi/covid/mapper/MapMapper.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/mapper/MapMapper.java
@@ -1,22 +1,14 @@
 package com.multi.covid.mapper;
 
-import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import com.multi.covid.domain.CenterVO;
-import com.multi.covid.domain.LiveVO;
-import com.multi.covid.domain.ResultVO;
 
 @Mapper
 @Repository
 public interface MapMapper {
-	ResultVO getOneResult(String date);
-	List<ResultVO> getBetweenResult(HashMap<String, String> map);
-	List<ResultVO> getAllResult();
-	LiveVO getOneLive(String date);
 	List<CenterVO> getAllCenter(Double lat,Double lng);
-	List<CenterVO> getAllCentertemp(Double lat,Double lng);
 }
