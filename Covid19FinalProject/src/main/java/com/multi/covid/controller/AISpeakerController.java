7bb5f475a00d2782bb87e74a28d3687a8414b8b2
louis-25 <20151577@vision.hoseo.edu>
2021-06-05 16:10:49 +0900

    코드정리

--- Covid19FinalProject/src/main/java/com/multi/covid/controller/AISpeakerController.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/controller/AISpeakerController.java
@@ -18,23 +18,21 @@ public class AISpeakerController {
 	private AISpeakerService service;	
 	
 	@ResponseBody
-	@RequestMapping("/patient") //일일확진자 조회	
+	@RequestMapping("/patient") //일일확진자 및 누적확진자 조회
 	public String getPatient(String day, String location) {
 		return service.getPatient(day, location);
 	}
 	
 	@ResponseBody
-	@RequestMapping("/triage") //가까운 선별진료소 조회
-	public String getGeolocation(String r1, String r2, String r3) { // r1 - 시, r2 - 구, r3 - 동
-		//스피커 접속지역의 선별진료소 목록을 반환한다
-		List<String> list = service.getGeolocation(r1, r2, r3);	
+	@RequestMapping("/vaccine-center") //스피커 접속지역의 가까운 선별진료소 조회
+	public String getVaccineCenter(String r1, String r2, String r3) { // r1 - 시, r2 - 구, r3 - 동
+		List<String> list = service.getVaccineCenter(r1, r2, r3);	
 		return list.get(0);
 	}
 	
 	@ResponseBody
-	@RequestMapping("/weather") //특정 지역의 날씨정보 조회
+	@RequestMapping("/weather") //스피커 접속지역의 날씨정보 조회
 	public String getWeather(String lat, String lon) throws IOException, ParseException {
-		//스피커 접속지역의 날씨정보를 반환한다		
 		return service.getWeather(lat, lon);
 	}	
 }
--- Covid19FinalProject/src/main/java/com/multi/covid/service/AISpeakerService.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/service/AISpeakerService.java
@@ -8,7 +8,7 @@ import org.json.simple.parser.ParseException;
 public interface AISpeakerService {	
 	
 	String getPatient(String day, String location); //일일 확진자수	
-	List<String> getGeolocation(String r1, String r2, String r3); //가까운 진료소
+	List<String> getVaccineCenter(String r1, String r2, String r3); //가까운 진료소
 	String getWeather(String lat, String lon) throws IOException, ParseException; //스피커 접속지역의 날씨정보	
 	
 }
