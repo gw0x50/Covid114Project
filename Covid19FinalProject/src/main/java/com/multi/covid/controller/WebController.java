115494e9449dd776d9705cfaa4addf6e083d8070
melonbottle <melonbottle@gmail.com>
2021-06-05 20:28:54 +0900

    WebController.java 간략화

--- Covid19FinalProject/src/main/java/com/multi/covid/controller/WebController.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/controller/WebController.java
@@ -1,30 +1,15 @@
 package com.multi.covid.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.multi.covid.domain.CenterVO;
-import com.multi.covid.service.MapService;
-
 @Controller
 public class WebController {
-	
-	
-	 @Autowired 
-	 private MapService mapService;
-	 
-	 
+
 	@RequestMapping("/")
 	public ModelAndView main() {
 		ModelAndView mv = new ModelAndView();
-		
-		
-		
-		 
 
 		mv.setViewName("index");
 
