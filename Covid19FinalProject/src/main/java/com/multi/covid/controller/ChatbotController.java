cb9a14f4473124b83e2bf70a7172e539984c1a8b
melonbottle <melonbottle@gmail.com>
2021-06-05 20:11:30 +0900

    chatbot 코드, 주석 맞춤법 및 들여쓰기 수정

--- Covid19FinalProject/src/main/java/com/multi/covid/controller/ChatbotController.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/controller/ChatbotController.java
@@ -1,6 +1,5 @@
 package com.multi.covid.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,55 +12,55 @@ import com.multi.covid.service.ChatbotService;
 @RequestMapping("/chatbot")
 public class ChatbotController {
 	@Autowired
-	private ChatbotService service;	
-	
+	private ChatbotService service;
+
 	//누적 확진자 조회
-	@RequestMapping(value="/getResult", method=RequestMethod.POST)
+	@RequestMapping(value = "/getResult", method = RequestMethod.POST)
 	@ResponseBody
 	public String getResult(@RequestBody String location) {
-		return service.getResult(location);	
-	}	
-		
+		return service.getResult(location);
+	}
+
 	//실시간 확진자 조회
-	@RequestMapping(value="/getLive", method=RequestMethod.POST)
+	@RequestMapping(value = "/getLive", method = RequestMethod.POST)
 	@ResponseBody
 	public String getLive(@RequestBody String location) {
 		return service.getLive(location);
 	}
-		
+
 	//백신 센터 리스트(지역별) 
-	@RequestMapping(value="/getCenterList", method=RequestMethod.POST)
+	@RequestMapping(value = "/getCenterList", method = RequestMethod.POST)
 	@ResponseBody
-	public String getCenterList(@RequestBody String location) {	
+	public String getCenterList(@RequestBody String location) {
 		return service.getCenterList(location);
 	}
-	
+
 	//백신 센터 선택지 return 
-	@RequestMapping(value="/selectAddr", method=RequestMethod.POST)
+	@RequestMapping(value = "/selectAddr", method = RequestMethod.POST)
 	@ResponseBody
 	public String selectAddr(@RequestBody String location) {
-		return service.selectAddr(location);		
+		return service.selectAddr(location);
 	}
-				
+
 	//백신 센터 주소 링크 반환
-	@RequestMapping(value="/getCenterUrl", method=RequestMethod.POST)
+	@RequestMapping(value = "/getCenterUrl", method = RequestMethod.POST)
 	@ResponseBody
 	public String getCenterUrl(@RequestBody String address) {
-		return service.getCenterUrl(address, 1);	
+		return service.getCenterUrl(address, 1);
 	}
-	
+
 	//백신 센터 주소 링크 반환(5 초과)
-	@RequestMapping(value="/getCenterUrlOver", method=RequestMethod.POST)
+	@RequestMapping(value = "/getCenterUrlOver", method = RequestMethod.POST)
 	@ResponseBody
 	public String getCenterUrlOver(@RequestBody String address) {
-		return service.getCenterUrl_over(address);	
+		return service.getCenterUrl_over(address);
 	}
-	
+
 	//기관 이름 직접 조회(주소 링크 반환)
-	@RequestMapping(value="/facilityCheck", method=RequestMethod.POST)
+	@RequestMapping(value = "/facilityCheck", method = RequestMethod.POST)
 	@ResponseBody
 	public String facilityCheck(@RequestBody String facility_name) {
-		return service.facilityCheck(facility_name);	
+		return service.facilityCheck(facility_name);
 	}
-	  
+
 }
--- Covid19FinalProject/src/main/java/com/multi/covid/mapper/ChatbotMapper.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/mapper/ChatbotMapper.java
@@ -12,13 +12,22 @@ import com.multi.covid.domain.ResultVO;
 @Repository
 public interface ChatbotMapper {
 	ResultVO getResult(String location); // 누적 확진자 수 조회 
+
 	LiveVO getLive(String date); // 실시간 확진자 수 조회 
+
 	int getLocLive(HashMap<String, String> map); // 실시간 지역 확진자 수 조회 
+
 	List<CenterVO> getLocCenter(String location); // 지역별 센터 조회 
+
 	List<String> getAddrTwo(String location); // 주소 두 번째 값 조회
+
 	List<String> getAddrThree(String address_two); //주소 세 번째 값 조회 
+
 	List<String> getAddrFour(String address_three); //주소 네 번째 값 조회 
+
 	List<CenterVO> getAddrCenter(String address_three); // 주소로 백신 센터 조회
+
 	List<CenterVO> getFacility(String facility_name); // 시설 이름으로 백신 센터 조회
+
 	List<CenterVO> getFacility_loc(HashMap<String, String> map); //시설, 지역 이름으로 백신 센터 조회 
 }
