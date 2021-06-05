aaa4ef12aa0f335a2d271212e108d3bbe8abe12b
louis-25 <20151577@vision.hoseo.edu>
2021-06-05 11:34:53 +0900

    소스코드 정리

--- Covid19FinalProject/src/main/java/com/multi/covid/controller/AISpeakerController.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/controller/AISpeakerController.java
@@ -18,21 +18,21 @@ public class AISpeakerController {
 	private AISpeakerService service;	
 	
 	@ResponseBody
-	@RequestMapping("/patient")//일일확진자	
-	public String getPatient(String day, String location) {//(today / yesterday)
+	@RequestMapping("/patient") //일일확진자 조회	
+	public String getPatient(String day, String location) {
 		return service.getPatient(day, location);
 	}
 	
 	@ResponseBody
-	@RequestMapping("/triage") //가까운 선별진료소
+	@RequestMapping("/triage") //가까운 선별진료소 조회
 	public String getGeolocation(String r1, String r2, String r3) { // r1 - 시, r2 - 구, r3 - 동
 		//스피커 접속지역의 선별진료소 목록을 반환한다
-		List<String> list = service.getGeolocation(r1, r2, r3);		
+		List<String> list = service.getGeolocation(r1, r2, r3);	
 		return list.get(0);
 	}
 	
-	@ResponseBody //classpath 수정
-	@RequestMapping("/weather") //특정 지역의 날씨정보
+	@ResponseBody
+	@RequestMapping("/weather") //특정 지역의 날씨정보 조회
 	public String getWeather(String lat, String lon) throws IOException, ParseException {
 		//스피커 접속지역의 날씨정보를 반환한다		
 		return service.getWeather(lat, lon);
