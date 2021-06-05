9fb2f04e1c11d2c32e0cc1c0f6eaae6a6f927d4f
2damlee <2damlee@gmail.com>
2021-06-05 13:34:32 +0900

    Controller, Mapper 주석 수정 및 추가

--- Covid19FinalProject/src/main/java/com/multi/covid/controller/ChatbotController.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/controller/ChatbotController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.multi.covid.service.ChatbotService;
 
 @Controller
@@ -22,24 +21,21 @@ public class ChatbotController {
 	public String getResult(@RequestBody String location) {
 		return service.getResult(location);	
 	}	
-	
-	
+		
 	//실시간 확진자 조회
 	@RequestMapping(value="/getLive", method=RequestMethod.POST)
 	@ResponseBody
 	public String getLive(@RequestBody String location) {
 		return service.getLive(location);
 	}
-	
-	
-	//백신 센터 리스트 
+		
+	//백신 센터 리스트(지역별) 
 	@RequestMapping(value="/getCenterList", method=RequestMethod.POST)
 	@ResponseBody
 	public String getCenterList(@RequestBody String location) {	
 		return service.getCenterList(location);
 	}
 	
-
 	//백신 센터 선택지 return 
 	@RequestMapping(value="/selectAddr", method=RequestMethod.POST)
 	@ResponseBody
@@ -47,35 +43,35 @@ public class ChatbotController {
 		return service.selectAddr(location);		
 	}
 	
-	//백신 센터 나머지 선택지 return 
+	//백신 센터 주소 20번째부터 나머지 선택지 return 
 	@RequestMapping(value="/selectAddrRemainder", method=RequestMethod.POST)
 	@ResponseBody
 	public String selectAddrRemainder(@RequestBody String location) {
 		return service.selectAddrRemainder(location);		
 	}	
 				
-	//백신 센터 조회(시 군 읍) + 링크
+	//백신 센터 주소 링크 반환
 	@RequestMapping(value="/getCenterUrl", method=RequestMethod.POST)
 	@ResponseBody
 	public String getCenterUrl(@RequestBody String address) {
 		return service.getCenterUrl(address, 1);	
 	}
 	
-	//백신 센터 조회(시 군 읍) + 링크 (5 초과 10 이하)
+	//백신 센터 주소 링크 반환(5 초과 10 이하)
 	@RequestMapping(value="/getCenterUrl10", method=RequestMethod.POST)
 	@ResponseBody
 	public String getCenterUrl10(@RequestBody String address) {
 		return service.getCenterUrl_over10(address);	
 	}
 	
-	//백신 센터 조회(시 군 읍) + 링크 (15 이상)
+	//백신 센터 주소 링크 반환(15 이상)
 	@RequestMapping(value="/getCenterUrl15", method=RequestMethod.POST)
 	@ResponseBody
 	public String getCenterUrl15(@RequestBody String address) {
 		return service.getCenterUrl_over15(address);	
 	}
 	
-	//기관 이름 직접 조회(주소링크 retrun)
+	//기관 이름 직접 조회(주소 링크 반환)
 	@RequestMapping(value="/facilityCheck", method=RequestMethod.POST)
 	@ResponseBody
 	public String facilityCheck(@RequestBody String facility_name) {
