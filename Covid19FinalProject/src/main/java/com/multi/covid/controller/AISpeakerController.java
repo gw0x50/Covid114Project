285290eac285c7b1435cdd76f659f68eb5402b4d
melonbottle <melonbottle@gmail.com>
2021-06-07 23:42:01 +0900

    주석 위치 수정

--- Covid19FinalProject/src/main/java/com/multi/covid/controller/AISpeakerController.java
+++ Covid19FinalProject/src/main/java/com/multi/covid/controller/AISpeakerController.java
@@ -16,20 +16,24 @@ import com.multi.covid.service.AISpeakerService;
 public class AISpeakerController {
 	@Autowired
 	private AISpeakerService service;
-
-	@RequestMapping(value = "/getPatient", method = RequestMethod.POST) // 일일 확진자 및 누적 확진자 조회
+	
+	// 일일 확진자 및 누적 확진자 조회
+	@RequestMapping(value = "/getPatient", method = RequestMethod.POST)
 	@ResponseBody
 	public String getPatient(String day, String location) {
 		return service.getPatient(day, location);
 	}
-
-	@RequestMapping(value = "/getVaccineCenter", method = RequestMethod.POST) // 스피커 접속지역의 가까운 선별진료소 조회
+	
+	// 스피커 접속지역의 가까운 선별진료소 조회
+	@RequestMapping(value = "/getVaccineCenter", method = RequestMethod.POST)
 	@ResponseBody
-	public String getVaccineCenter(String r1, String r2, String r3) { // r1 - 시, r2 - 구, r3 - 동		
+	public String getVaccineCenter(String r1, String r2, String r3) { 
+		// r1 - 시, r2 - 구, r3 - 동
 		return service.getVaccineCenter(r1, r2, r3);
 	}
-
-	@RequestMapping(value = "/getWeather", method = RequestMethod.POST) // 스피커 접속지역의 날씨 정보 조회
+	
+	// 스피커 접속지역의 날씨 정보 조회
+	@RequestMapping(value = "/getWeather", method = RequestMethod.POST)
 	@ResponseBody
 	public String getWeather(String lat, String lon) throws IOException, ParseException {
 		return service.getWeather(lat, lon);
